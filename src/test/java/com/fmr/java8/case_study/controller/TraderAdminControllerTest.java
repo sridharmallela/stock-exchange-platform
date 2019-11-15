14f0c606206cd16d028157b13c2cf4848fb1695d
chunleidu100 <chunleidu100@gmail.com>
2019-11-15 10:25:39 -0500

    added unit test function findTraderByLastName

--- src/test/java/com/fmr/java8/case_study/controller/TraderAdminControllerTest.java
+++ src/test/java/com/fmr/java8/case_study/controller/TraderAdminControllerTest.java
@@ -1,11 +1,19 @@
 package com.fmr.java8.case_study.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.fmr.java8.case_study.domain.Trader;
+
+import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsInAnyOrder;
@@ -39,6 +47,23 @@ class TraderAdminControllerTest {
 
     @Test
     void findTraderByLastName() {
+    	Response response =        
+	    	given()
+	        .accept(MediaType.APPLICATION_JSON_VALUE)
+	        .when()
+	        .get("/admin/traders/name/Garrett")
+	        .then()
+	        .statusCode(HttpStatus.OK.value())
+	        .extract()
+	        .response();  
+        
+        List<Trader> traders = response.as(List.class);
+        //assertEquals(2, traders.length);
+        
+        assertEquals(2, traders.size());     
+        
+        
+        
     }
 
     @Test
